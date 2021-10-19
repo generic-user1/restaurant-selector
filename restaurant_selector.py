85c8ee4b17b06accc571c5ba68fc112b2fb70811
generic-user1 <89677116+generic-user1@users.noreply.github.com>
2021-10-19 00:08:37 -0400

    Added search expanding to interactivePrompt
    excludeId handling needs work

--- restaurant_selector.py
+++ restaurant_selector.py
@@ -1,8 +1,10 @@
-
-
 #generates a request for a Google Maps API geolocation service
 #this has to be a POST request and as such a simple URL will not suffice
 #returns the generated request without sending it
+from os import close
+from types import resolve_bases
+
+
 def generateGeolocationRequest():
     from urllib.request import Request
     from key_management import getApiKey
@@ -58,9 +60,11 @@ def generatePlaceSearchRequest(latitude, longitude, searchText = "restaurant", r
 
     queryParam = f"query={encodeForUrl(searchText)}"
 
+    radiusParam = f"radius={range}"
+
     keyParam = f"key={apikey}"
 
-    requestText = f"{urlBase}{locationParam}&{queryParam}&{keyParam}"
+    requestText = f"{urlBase}{locationParam}&{queryParam}&{radiusParam}&{keyParam}"
 
     return requestText
 
@@ -75,10 +79,9 @@ def executePlaceSearchRequest(searchRequestText = None, locationData = None):
     if searchRequestText == None and locationData == None:
         locationData = executeGeolocationRequest()
 
-    latitude = locationData['location']['lat']
-    longitude = locationData['location']['lng']
-
     if searchRequestText == None:
+        latitude = locationData['location']['lat']
+        longitude = locationData['location']['lng']
         searchRequestText = generatePlaceSearchRequest(latitude, longitude)
 
     searchResponse = urlopen(searchRequestText)
@@ -90,29 +93,79 @@ def executePlaceSearchRequest(searchRequestText = None, locationData = None):
     except JSONDecodeError:
         print(f"Attempted to parse {searchResponseText} as JSON, but it was invalid")
         raise
-    
+
+#given a dict from executePlaceSearchRequest, attempts to return 
+#the 'results' list. If a key error occurs (most likely due to unexpected
+# API behavior), returns unmodified input
+#if input is already a list, returns it unmodified
+def getSearchResultsAsList(searchResults):
+
+    #test if list
+    if isinstance(searchResults, list):
+        #if input is a list, return it immediately
+        return searchResults
+
+    #if not a list, attempt to get as a list
+    try:
+        resultList = searchResults['results']
+    except KeyError:
+        #if a key error occurs, return unmodified input
+        resultList = searchResults
+
+    return resultList
+
+#given a list of restaurants, returns only those 
+#which are open currently
+#if excludePlaceIds is set to a list of place ids,
+#those places will not be included in the list 
+def getOpenRestaurants(restaurantList, excludePlaceIds=None):
+
+    #ensure input is a list
+    restaurantList = getSearchResultsAsList(restaurantList)
+
+    openRestaurantList = []
+
+    #iterate through list of places to find ones that are open
+    for placeData in restaurantList:
+
+        if placeData['opening_hours']['open_now']:
+            #if excludePlaceIds is none, skip checking place id
+            if excludePlaceIds == None:
+                openRestaurantList.append(placeData)
+            else:
+                #if excludePlaceIds is set, ensure this place id 
+                #is not in it before appending
+                if placeData['place_id'] not in excludePlaceIds:
+                    openRestaurantList.append(placeData)
+
+
+    return openRestaurantList
+
 
 #given the results of a search request,
-#picks out one location that is currently open
-#and returns all details on it
-#if onlyOpen is false, skips checking for open status
-def pickRestaurant(searchResults, onlyOpen = True):
-    from random import choice
+#picks out one location and returns its index in the search results
+#Does NOT return the actual item; if you want to return the item
+#instead of an index, use the pickRestaurant function instead (defined below)
+#if excludeIds is set to a list of placeIds, places with those ids won't be chosen
+def pickRestaurantIndex(searchResults, excludeKeys=None):
+    from random import randrange
     
-    relevantPlaces = []
+    #get search results as list, if it wasn't one already
+    searchResultsList = getSearchResultsAsList(searchResults)
+    
+    #pick a random place's index
+    return randrange(0, len(searchResultsList))
 
-    if not onlyOpen:
-        #if onlyOpen is false, all places are relevant
-        relevantPlaces = searchResults['results']
-    else:
-        #iterate through list of places to find ones that are open
-        for placeData in searchResults['results']:
 
-            if placeData['opening_hours']['open_now']:
-                relevantPlaces.append(placeData)
+#uses pickRestaurantIndex to select a random
+#restaurant, then return the details of the restaurant
+#see pickRestaurantIndex definition for more details
+def pickRestaurant(searchResults):
 
-    #pick a random place out of the list of relevant ones
-    return choice(relevantPlaces)
+    searchResultsList = getSearchResultsAsList(searchResults)
+    restaurantIndex = pickRestaurantIndex(searchResultsList)
+    return searchResultsList[restaurantIndex]
+
 
 #given a place id (from the Maps API),
 #return a URL that points to that location
@@ -125,6 +178,14 @@ def getPlaceLink(placeId):
     placeLink = f"https://www.google.com/maps/place/?q=place_id:{placeId}"
     return placeLink
 
+#generates and prints a Google Maps link to a place, given a place id
+#surrounds it with space using newlines so it's easier to copy+paste
+def printPlaceLink(placeId):
+    
+    placeLink = getPlaceLink(placeId)
+    print(f"\n\n{placeLink}\n\n")
+
+
 #given a Place object (as returned by the pickRestaurant function),
 #print Place information that is relevant to the user to the console
 #(this includes the name, full address, rating, price level, and Google Maps URL)
@@ -136,21 +197,228 @@ def printInfoForUser(selectedPlace):
     #print the place address
     print(f"{selectedPlace['formatted_address']}")
 
-    #print the place's rating along with the number of reviews
-    print(f"Rated {selectedPlace['rating']} out of 5 stars (from {selectedPlace['user_ratings_total']} reviews)")
+    #print the place's price level, if it exists
+    try:
+        print(f"Price level: {selectedPlace['price_level']} out of 3")
+    except KeyError:
+        print("Price level unknown")
 
-    #print the link to the place on Google Maps
-    placeLink = getPlaceLink(selectedPlace['place_id'])
-    print(f"\n\n{placeLink}\n\n")
+    try:
+        #print the place's rating along with the number of reviews
+        print(f"Rated {selectedPlace['rating']} out of 5 stars (from {selectedPlace['user_ratings_total']} reviews)")
+    except KeyError:
+        print("Rating unknown")
 
-#define a testing method
-def restaurant_selector_test():
-    restaurants = executePlaceSearchRequest()
+        #print an empty line to signify the end of info
+        print()
 
-    selectedRestaurant = pickRestaurant(restaurants)
 
-    printInfoForUser(selectedRestaurant)
+#asks the user for a yes or no
+#uses a loop to repeat the question if input is invalid
+#returns a boolean true/false representing the user's choice
+def promptYesNo(promptText):
+    
+    #input validation loop
+    #when valid input is recieved, the function
+    #returns, exiting this loop
+    while True:
+        #prompt user for input
+        #this is done inside a loop so that the prompt can be repeated 
+        userInput = input(promptText)
+        
+        #store user input in lowercase
+        #all checks against the value of the userInput
+        #are done against this, meaning the actual case of the letters
+        #the user typed doesn't matter 
+        lowercaseUserInput = userInput.lower()
 
-#test this module if run as a script
+        #'parse' user input into a boolean 
+        if lowercaseUserInput in ('y', 'yes'):
+            return True
+        elif lowercaseUserInput in ('n', 'no'):
+            return False
+        else:
+            #print message and continue loop if input invalid
+            print(f"Invalid input: {userInput}")
+
+
+#ask the user if the specified location is acceptable
+#prompt for a (y)es or (n)o
+#handles invalid input by repeating the prompt
+def askIfAcceptable(restaurantName = None):
+
+    #set restaurantName to something generic if no name was specified
+    if restaurantName == None:
+        restaurantName = "this restaurant"
+
+    promptText = f"Is {restaurantName} acceptable? (input 'y' for yes or 'n' for no): "
+
+    return promptYesNo(promptText)
+
+
+#returns the results of a Maps request for use with the interactivePrompt
+def getNearbyRestaurants(userLocation=None, searchRange = 10000, searchText = "restaurant"):
+    
+    if userLocation == None:
+        #get the user's location if it wasn't provided
+        userLocation = executeGeolocationRequest()['location']
+    else:
+        #attempt to extract location data from a geolocation response
+        try:
+            userLocation = userLocation['location']
+        except KeyError:
+            #if a key error is raised, it's possible the location data
+            #is already in the correct format.
+            #if the proper keys are in this dict,
+            #the KeyError is suppressed
+            locationKeys = userLocation.keys()
+            if 'lat' in locationKeys and 'lng' in locationKeys:
+                pass
+            else:
+                #if either key is missing, the event is not suppressed
+                raise
+
+    #generate a search request
+    #this is done outside of executePlaceSearchRequest so that adding more options
+    #(such as range limiting or different search text) is easier
+    searchRequest = generatePlaceSearchRequest(
+        userLocation['lat'], userLocation['lng'], 
+        searchText, searchRange)
+
+    restaurants = executePlaceSearchRequest(searchRequest)
+
+    return restaurants
+
+
+#an interactive prompt to select a restaurant
+#- userLocation param, if set, is used as the user's location data
+#  if not set, the result of executeGeolocationRequest is used instead
+#
+#- includeClosed param is a boolean representing whether or not to include
+#  closed locations in the output
+#  note: if the user rejects every open location, they will be prompted to 
+#  change this option during runtime. This does NOT call any APIs again
+#
+#- searchRange param is the range to search for in meters
+#  note: if the user rejects every open location, and either chooses not to 
+#  include closed locations or has already exhausted the closed locations,
+#  they will be prompted to expand the search range. If they choose yes,
+#  ***this DOES call the Places TextSearch API again*** 
+#  If you do not want to offer this option to the user,
+#  set the restrictedRange parameter to True
+#  
+#  -restrictedRange param, if True, disables the prompt to increase
+#  search radius (restricting the number of API calls)
+#
+#  -excludeIds param is a list of Place Ids to exclude from results.
+#  It is set to None by default, representing an empty list
+def interactivePrompt(userLocation = None,  includeClosed = False,
+    searchRange = 10000, restrictedRange = False, excludeIds = None):
+
+    #execute relevant requests if list of restaurants is not provided
+    if userLocation == None:
+        userLocation = executeGeolocationRequest()
+    
+    restaurants = getNearbyRestaurants(userLocation, searchRange)
+
+    #convert restaurants to list, if it wasn't one already
+    restaurants = getSearchResultsAsList(restaurants)
+
+    if not includeClosed:
+        #narrow down restaurants to only those which are currently open
+        #also use excludeIds; this is where they will be filtered
+        #TODO: break excludeId handling into its own function
+        relevantRestaurants = getOpenRestaurants(restaurants, excludeIds)
+        
+        #get a list of closed restaurants (to expand search with, if needed)
+        closedRestaurants = []
+        for restaurant in restaurants:
+            if restaurant not in relevantRestaurants:
+                closedRestaurants.append(restaurant)
+
+    else:
+        #if includeClosed is true, all restaurants are relevant
+        #closedRestaurants is not set in this case as it would never be used
+        relevantRestaurants = restaurants    
+
+    #define list to track restaurants that have already been viewed
+    #place ids are used because indexes or internal ids will not be
+    #consistent over multiple restaurant search results
+    viewedRestaurantIds = []
+
+    print("You should eat at:")
+
+    #loop until user likes what was selected
+    userLikesChoice = False
+    while not userLikesChoice:
+
+        #pick a restaurant index at random
+        selectedRestaurantIndex = pickRestaurantIndex(relevantRestaurants)
+        #pop that restaurant from the list so that it isn't repeated
+        selectedRestaurant = relevantRestaurants.pop(selectedRestaurantIndex)
+
+        #add the place id of this restaurant
+        #to the list of viewed restaurant ids
+        viewedRestaurantIds.append(selectedRestaurant['place_id'])
+
+        #print info on the restaurant that 
+        #would be relevant to the user
+        printInfoForUser(selectedRestaurant)
+
+        #prompt the user if the selected restaurant is acceptable
+        userLikesChoice = askIfAcceptable(selectedRestaurant['name'])
+
+        #if the user accepts the restaurant, print a Google Maps link
+        #to the location, then return the selected restaurant
+        if userLikesChoice:
+            print("Fortunate! Here is a link:")
+            printPlaceLink(selectedRestaurant['place_id'])
+            return selectedRestaurant
+        else:
+            #continue if there are more restaurants in the list
+            if len(relevantRestaurants) > 0:
+                print("\nUnfortunate! How about:") 
+                continue  
+            else:
+                #if restaurant list is empty, ask to change options to include more restaurants
+                print("Super Unfortunate! You have exhausted the restaurants in your area.")
+
+                #prompt user to expand search to closed locations,
+                #if they weren't already included
+                if not includeClosed:
+                    if promptYesNo("Include closed locations in results? (y/n): "):
+                        #set includeClosed so this prompt is not shown again
+                        includeClosed = True
+                        #set the relevantRestaurants list
+                        #to the closed restaurants that were not shown earlier
+                        relevantRestaurants = closedRestaurants
+                        #continue loop to iterate through closedRestaurants
+                        print("How about:")
+                        continue
+                
+                #prompt user to expand search area if restrictedRange is False
+                #(this requires an additional Maps API call if y is selected)
+                if not restrictedRange:
+                    if promptYesNo("Expand search area? (y/n): "):
+                        
+                        #if user chooses to expand search area, 
+                        #restart the prompt with the same location but 
+                        #a bigger search range. also set the excludeIds param to
+                        #the already viewed restaurant ids
+                        #note that includeClosed is always set to false
+                        #doing this recursively is probably a bad idea, 
+                        #but *should* be fine, at least for a few expansions
+                        #TODO: refactor interactivePrompt so recursion isn't required
+                        return interactivePrompt(userLocation, False, searchRange*2, 
+                            excludeIds=viewedRestaurantIds)
+                        
+                #return None if no restaurant was selected and 
+                #all options to expand have been exhausted
+                return None
+
+
+
+#if this module is called as a script, run the
+#interactive prompt
 if __name__ == "__main__":
-    restaurant_selector_test()
+    interactivePrompt()
